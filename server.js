a1f6fd43a1b2eac8fffe795dd002e834e539fc80
mightygaby <gaby.rf@gmail.com>
2017-07-04 00:13:49 -0500

    cleanup code

--- server.js
+++ server.js
@@ -20,16 +20,12 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-// app.use(express.static('web'));
 
 mongo.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@ds139072.mlab.com:39072/fertility_data').then(function(database){
 
  db = database;
  // Collections
- console.log('connected to ', database);
- test_db = db.collection('beantest');
+ test_db = db.collection('beantest'); //TODO: change this to real database after test
   // Start Server
   app.listen(3000, function() {
     console.log('Server: Running on port 3000');
@@ -47,23 +43,8 @@ app.post('/collect_data', function (req, res, next) {
       temp = request.temp;
 
 
-  //  test_db.findOne({uuid: 1234}).then(function(device) {
-  //     // if (device == 'null') {
-  //     //   console.log('null')
-  //     // } else if (device.uuid.length) {
-  //     //   console.log('yes')
-  //     // }
-  //     console.log(device)
-  //   }).catch(function(err) {
-  //     console.error("something went wrong");
-  //   })
-
-  // to do: 
+  // to do: handle new sensortag
   test_db.update({ 'uuid': uuid }, {'$push': {'data': {'timeStamp': timeStamp, 'temp': temp}}})
-
-  // get request data (req) coming from ajax
-  // store bean id in variable
-  // find bean id in mongodb - fertility_data.insert({user_id: bean_id_var, data})
 
   res.send('Got a POST request');
 })
